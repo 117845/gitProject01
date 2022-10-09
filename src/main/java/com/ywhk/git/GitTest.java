35051c7fcd0c8e12cb8d690f7eb66aa8e0c16fac
zhangwenwen <1178457982@qq.com>
2022-10-09 08:49:51 +0800

    second commit

--- src/main/java/com/ywhk/git/GitTest.java
+++ src/main/java/com/ywhk/git/GitTest.java
@@ -10,5 +10,6 @@ package com.ywhk.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git1!");
+        System.out.println("hello,git2!");
     }
 }
