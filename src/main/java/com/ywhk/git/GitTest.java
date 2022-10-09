74fc94266b3f03c6927486d84d6fe48a2efbc2a3
zhangwenwen <1178457982@qq.com>
2022-10-09 08:49:23 +0800

    first commit

---
+++ src/main/java/com/ywhk/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.ywhk.git;
+
+/**
+ * 功能描述:
+ *
+ * @author zhangwenwen
+ * @version 1.0
+ * @date 2022/10/9 8:47
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git1!");
+    }
+}
